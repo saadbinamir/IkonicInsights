a7274dca829c5432996446d8ec4112cee737c39c
saadbinamir <saad.amir28@gmail.com>
2024-01-25 18:56:43 +0500

    migrations

--- backend/database/migrations/2024_01_25_134651_create_feedback_table.php
+++ backend/database/migrations/2024_01_25_134651_create_feedback_table.php
@@ -11,8 +11,12 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('feedback', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')->constrained('users');
+            $table->string('title');
+            $table->text('description');
+            $table->string('category');
             $table->timestamps();
         });
     }
@@ -22,6 +26,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('feedback');
     }
 };
